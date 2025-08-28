f406705d4251d22d4d50e56d1bc6fd2f0ffdb12c
Michele Focchi <michele.focchi@unitn.it>
2025-08-28 13:03:58 +0000

    no TF errors victims.launch with one robot without controller

--- map_pkg/scripts/ground_truth_odom.py
+++ map_pkg/scripts/ground_truth_odom.py
@@ -6,30 +6,52 @@ from nav_msgs.msg import Odometry
 from tf.broadcaster import TransformBroadcaster
 import tf
 from geometry_msgs.msg import Quaternion
-
-ROBOT_NAME = rospy.get_param('~robot_name', 'turtlebot3_burger')
+import numpy as np
 
 
 class GroundTruthOdom:
     def __init__(self):
-        self.odom_pub = rospy.Publisher('/odom', Odometry, queue_size=10)
+
         self.br = TransformBroadcaster()
         rospy.Subscriber('/gazebo/model_states', ModelStates, self.callback)
+        self.ROBOT_NAME = rospy.get_param('~robot_name', 'turtlebot3_burger')
+        self.odom_pub = rospy.Publisher(self.ROBOT_NAME+'/odom', Odometry, queue_size=1)
+    
+
+        self.tf_rate = rospy.get_param('~rate', 30.0)
+        self.publish_period = rospy.Duration(1.0 / self.tf_rate)
+        self.last_pub = rospy.Time(0)
+        self.last_stamp = rospy.Time(0)
 
     def callback(self, msg):
         try:
-            idx = msg.name.index(ROBOT_NAME)
+            idx = msg.name.index(self.ROBOT_NAME)
         except ValueError:
-            rospy.logwarn("Robot model not found in /gazebo/model_states")
+            rospy.logwarn(f"Robot model {self.ROBOT_NAME} not found in /gazebo/model_states")
             return
+
+        #go at lower rate
+        now = rospy.Time.now()
+
+        # throttle
+        if now - self.last_pub < self.publish_period:
+            return
+        self.last_pub = now
+
+        # enforce monotonic stamps (handles sim-time repeats)
+        if now <= self.last_stamp:
+            now = self.last_stamp + rospy.Duration.from_sec(1e-4)
+        self.last_stamp = now
+
+
 
         pose = msg.pose[idx]
         twist = msg.twist[idx]
 
         odom_msg = Odometry()
-        odom_msg.header.stamp = rospy.Time.now()
-        odom_msg.header.frame_id = "odom"
-        odom_msg.child_frame_id = "base_link"
+        odom_msg.header.stamp = now
+        odom_msg.header.frame_id = f"{self.ROBOT_NAME}/odom"
+        odom_msg.child_frame_id = f"{self.ROBOT_NAME}/base_link"
 
         odom_msg.pose.pose = pose
         odom_msg.twist.twist = twist
@@ -41,9 +63,10 @@ class GroundTruthOdom:
             (pose.position.x, pose.position.y, pose.position.z),
             (pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w),
             odom_msg.header.stamp,
-            "base_link",
-            "odom"
+            f"{self.ROBOT_NAME}/base_link",
+            f"{self.ROBOT_NAME}/odom"
         )
+
 
 if __name__ == '__main__':
     rospy.init_node('ground_truth_odom')
